75f910ef468b78bb6f9c8b7b45de768edb2cafba
basdedas <567910@student.saxion.nl>
2024-11-21 18:44:46 +0100

    Implemented graphs for the hourly temperature and humidity over the day in MainWindow.xaml and MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,14 +1,14 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using sql_fetcher;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp;
+using Axis = LiveChartsCore.SkiaSharpView.Axis;
+
 
 namespace Weather_App
 {
@@ -17,8 +17,15 @@ namespace Weather_App
         //TODO: Fill in how much datapoints per hour are fetched into the database
         private readonly int DatapointsPerHour = 15;                         //Placeholder
         private static readonly sql_fetcher.DataAccess DataAccess = new DataAccess();
-        public string CurrentDay { get; set; } //Based on this we can access which day it is, so for last 7 days it is CurrentDay - 7, CurrentDay -6 ... CurrentDay
+        
+        //Graph stuff
+        public ISeries[] TemperatureDaySeries { get; private set; }                    //Series for the graph
+        public ISeries[] HumidityDaySeries { get; private set; }                       //Series for the graph
 
+        public Axis[] XAxes { get; private set; }
+
+        //Data
+        public string CurrentDay { get; set; } //Based on this we can access which day it is, so for last 7 days it is CurrentDay - 7, CurrentDay -6 ... CurrentDay
         public double CurrentTemperature { get; private set; }                 //Current temperature
         public List<double> DayTemperature { get; private set; }               //All temperature data of today
         public List<double> WeekTemperature { get; private set; }              //All temperature data of this week
@@ -66,6 +73,42 @@ namespace Weather_App
                 DailyWeekTemperatureAverage.Add(WeekTemperature.GetRange(i * 24 * DatapointsPerHour, 24 * DatapointsPerHour).Average());
                 DailyWeekHumidityAverage.Add(WeekHumidity.GetRange(i * 24 * DatapointsPerHour, 24 * DatapointsPerHour).Average());
             } //Gets the average of each day over the week and adds it to the list of averages.
+
+            TemperatureDaySeries =
+            [
+                new LineSeries<double>
+                {
+                    Values = HourlyDayTemperatureAverage,
+                    Fill = null,
+                    Stroke = new SolidColorPaint(SKColors.Red),
+                    GeometrySize = 10,
+                    Name = "Temperature"
+                }
+            ];
+            
+            HumidityDaySeries =
+            [
+                new LineSeries<double>
+                {
+                    Values = HourlyDayHumidityAverage,
+                    Fill = null,
+                    Stroke = new SolidColorPaint(SKColors.SkyBlue),
+                    GeometrySize = 10,
+                    Name = "Humidity"
+                }
+            ];
+            
+            XAxes =
+            [
+                new Axis
+                {
+                    Labels =
+                    [
+                        "00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"
+                    ],
+                    Name = "Time"
+                }
+            ];
         }
     }
 }
