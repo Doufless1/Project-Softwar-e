fba0b6e9f16ca2d5c732f5c95b02d5d18bb9c9e1
basdedas <567910@student.saxion.nl>
2024-11-26 10:29:55 +0100

    Changed the axis and implemented a grid in the MainWindow.xaml

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,8 +21,13 @@ namespace Weather_App
         //Graph stuff
         public ISeries[] TemperatureDaySeries { get; private set; }                    //Series for the graph
         public ISeries[] HumidityDaySeries { get; private set; }                       //Series for the graph
-
-        public Axis[] XAxes { get; private set; }
+        public ISeries[] TemperatureWeekSeries { get; private set; }                    //Series for the graph
+        public ISeries[] HumidityWeekSeries { get; private set; }                       //Series for the graph
+        public ISeries[] TemperatureMonthSeries { get; private set; }                    //Series for the graph
+        public ISeries[] HumidityMonthSeries { get; private set; }                       //Series for the graph
+        public Axis[] XAxesDay { get; private set; }
+        public Axis[] XAxesWeek { get; private set; }
+        public Axis[] XAxesMonth { get; private set; }
 
         //Data
         public string CurrentDay { get; set; }                                  //Based on this we can access which day it is, so for last 7 days it is CurrentDay - 7, CurrentDay -6 ... CurrentDay
@@ -33,17 +38,20 @@ namespace Weather_App
         public double CurrentTemperature { get; private set; }                 //Current temperature
         public List<double> DayTemperature { get; private set; }               //All temperature data of today
         public List<double> WeekTemperature { get; private set; }              //All temperature data of this week
+        public List<double> MonthTemperature { get; private set; }             //All temperature data of this month
         public List<double> HourlyDayTemperatureAverage { get; private set; }  //Average temperature of today (useful for graphing)
         public List<double> DailyWeekTemperatureAverage { get; private set; }  //Average humidity of this week (useful for graphing)
+        public List<double> DailyMonthTemperatureAverage { get; private set; } //Average temperature of this month (useful for graphing)
         
         public double CurrentHumidity { get; private set; }                    //Current humidity
         public List<double> DayHumidity { get; private set; }                  //All humidity of today
         public List<double> WeekHumidity { get; private set; }                 //All humidity of this week
+        public List<double> MonthHumidity { get; private set; }                //All humidity of this month
         public List<double> HourlyDayHumidityAverage { get; private set; }     //Average humidity of today (useful for graphing)
         public List<double> DailyWeekHumidityAverage { get; private set; }     //Average humidity of this week (useful for graphing)
-        
+        public List<double> DailyMonthHumidityAverage { get; private set; } //Average temperature of this month (useful for graphing)
         //GUI
-        public List<Button> LocationButtons { get; private set; }                            //Buttons for the locations
+        public List<Button> LocationButtons { get; private set; }               //Buttons for the locations
         
         public MainWindow()
         {
@@ -61,16 +69,21 @@ namespace Weather_App
             
             DailyWeekTemperatureAverage = new List<double>();
             DailyWeekHumidityAverage = new List<double>();
+            
+            DailyMonthTemperatureAverage = new List<double>();
+            DailyMonthHumidityAverage = new List<double>();
 
             //Simple GetData
             CurrentTemperature = DataAccess.GetData(AccesableData.CurrentTemperature, Locations.Enschede)[0];
             DayTemperature = DataAccess.GetData(AccesableData.DayTemperature, Locations.Enschede);
             WeekTemperature = DataAccess.GetData(AccesableData.WeekTemperature, Locations.Enschede);
+            MonthTemperature = DataAccess.GetData(AccesableData.MonthHumidity, Locations.Enschede);
             
             CurrentHumidity = DataAccess.GetData(AccesableData.CurrentHumidity, Locations.Enschede)[0];
             DayHumidity = DataAccess.GetData(AccesableData.DayHumidity, Locations.Enschede);
             WeekHumidity = DataAccess.GetData(AccesableData.CurrentHumidity, Locations.Enschede);
-
+            MonthHumidity = DataAccess.GetData(AccesableData.CurrentHumidity, Locations.Enschede);
+            
             //Calculate averages
             for (int i = 0; i < 24; i++)
             {
@@ -83,7 +96,12 @@ namespace Weather_App
                 DailyWeekTemperatureAverage.Add(WeekTemperature.GetRange(i * 24 * DatapointsPerHour, 24 * DatapointsPerHour).Average());
                 DailyWeekHumidityAverage.Add(WeekHumidity.GetRange(i * 24 * DatapointsPerHour, 24 * DatapointsPerHour).Average());
             } //Gets the average of each day over the week and adds it to the list of averages.
-
+            for (int i = 0; i < 30; i++)
+            {
+                DailyMonthTemperatureAverage.Add(MonthTemperature.GetRange(i * 24 * DatapointsPerHour, 24 * DatapointsPerHour).Average());
+                DailyMonthHumidityAverage.Add(MonthHumidity.GetRange(i * 24 * DatapointsPerHour, 24 * DatapointsPerHour).Average());
+            } //Gets the average of each day over the month and adds it to the list of averages.
+            
             for (int i = 23; i >= 0; i++)
             {
                 Last24Hours[i] = DateTime.Now.AddHours(-i);
@@ -111,6 +129,30 @@ namespace Weather_App
                 }
             ];
             
+            TemperatureWeekSeries =
+            [
+                new LineSeries<double>
+                {
+                    Values = DailyWeekTemperatureAverage,
+                    Fill = null,
+                    Stroke = new SolidColorPaint(SKColors.Red),
+                    GeometrySize = 10,
+                    Name = "Temperature"
+                }
+            ];
+            
+            TemperatureMonthSeries =
+                [
+                    new LineSeries<double>
+                    {
+                        Values = DailyMonthTemperatureAverage,
+                        Fill = null,
+                        Stroke = new SolidColorPaint(SKColors.Red),
+                        GeometrySize = 10,
+                        Name = "Temperature"
+                    }
+                ];
+            
             HumidityDaySeries =
             [
                 new LineSeries<double>
@@ -123,22 +165,54 @@ namespace Weather_App
                 }
             ];
             
-            XAxes =
+            HumidityWeekSeries =
+            [
+                new LineSeries<double>
+                {
+                    Values = DailyWeekHumidityAverage,
+                    Fill = null,
+                    Stroke = new SolidColorPaint(SKColors.SkyBlue),
+                    GeometrySize = 10,
+                    Name = "Humidity"
+                }
+            ];
+            
+            HumidityMonthSeries =
+                [
+                    new LineSeries<double>
+                    {
+                        Values = DailyMonthHumidityAverage,
+                        Fill = null,
+                        Stroke = new SolidColorPaint(SKColors.SkyBlue),
+                        GeometrySize = 10,
+                        Name = "Humidity"
+                    }
+                ];
+            
+            XAxesDay =
             [
                 new Axis
                 {
                     Labels = Last24Hours.Select(x => x.ToString("HH:mm")).ToArray(),
                     Name = "Time"
-                },
+                }
+            ];
+            
+            XAxesWeek =
+            [
                 new Axis
                 {
                     Labels = Last7Days.Select(x => x.ToString("dd/MM")).ToArray(),
-                    Name = "7 Days"
-                },
+                    Name = "Time"
+                }
+            ];
+            
+            XAxesMonth =
+            [
                 new Axis
                 {
                     Labels = Last30Days.Select(x => x.ToString("dd/MM")).ToArray(),
-                    Name = "30 Days"
+                    Name = "Time"
                 }
             ];
 
