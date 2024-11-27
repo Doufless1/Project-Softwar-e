c731af33265d211540ca5d9bb2eeded42e2f9c57
Doufless1 <552608.student.saxion.nl>
2024-11-27 11:08:57 +0100

    I was pusshing DD instead of dd

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -159,7 +159,7 @@ namespace Weather_App
             {
                 new Axis
                 {
-                    Labels = Last7Days.Select(x => x.ToString("DD/MM")).ToArray(),
+                    Labels = Last7Days.Select(x => x.ToString("dd/MM")).ToArray(),
                     Name = "Date (DD/MM)"
                 }
             };
@@ -168,7 +168,7 @@ namespace Weather_App
             {
                 new Axis
                 {
-                    Labels = Last30Days.Select(x => x.ToString("DD/MM")).ToArray(),
+                    Labels = Last30Days.Select(x => x.ToString("dd/MM")).ToArray(),
                     Name = "Date (DD/MM)"
                 }
             };
